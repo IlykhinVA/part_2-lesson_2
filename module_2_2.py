85ac1952000b1fea7c74f6bad064b8d9ff68eb95
IlykhinVA <priest49175@gmail.com>
2024-08-31 08:41:13 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first=int(input('введите первое целое число: '))
+second=int(input('введите второе целое число: '))
+third=int(input('введите третье целое число: '))
+if first!=second and first!= third and second!=third:
+    print(0)
+elif first==second and first==third
+    print(3)
+else:
+    print(2)
